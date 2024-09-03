4d70afaab20fd09e02f61c91922cd1cf2fc438f9
AlizahIkram <abikram@alumni.uit.edu>
2024-09-03 08:59:11 -0700

    fixed

---
+++ src/AboutUs.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./AboutUs.css";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <p className="about-us-content">
+        Paradise Nursery is a vibrant and thriving online plant shop dedicated
+        to bringing the beauty of nature into your home. Specializing in a wide
+        variety of houseplants, we cater to plant enthusiasts of all levels,
+        from beginners to seasoned green thumbs. Our collection includes a
+        diverse range of plants, carefully selected for their quality and
+        aesthetic appeal. Whether you're looking for aromatic plants to freshen
+        up your living space, medicinal plants to enhance your wellness, or
+        simply a stunning centerpiece for your home, Paradise Nursery has
+        something for everyone.  
+      </p>
+      <p className="about-us-content">
+      We are committed to providing excellent customer service,
+        offering expert advice on plant care, and ensuring that every plant
+        arrives at your doorstep in perfect condition. Our shopping experience
+        is designed with you in mind, featuring a seamless navigation system, a
+        dynamic shopping cart, and detailed product listings that make it easy
+        to find exactly what you're looking for. At Paradise Nursery, we are
+        more than just a plant shop; we are your partners in creating a lush,
+        green oasis within your home.
+      </p>
+
+
+    </div>
+  );
+}
+
+export default AboutUs;
